023543a02fa0ab31cefd60e28b6e7c85aaa909d8
Christoffer Skog <skogh@newglue.com>
2022-10-31 07:30:05 +0100

    Initial commit

---
+++ apps/esmodules-repro/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'esmodules-repro-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'esmodules-repro';
+}
